07bf699e3700c208d047bfed916f66fd19d4aa2a
phil-bstorm <philippe.haerens@bstorm.be>
2025-03-09 21:11:06 +0100

    [Add] Chapter navigation

---
+++ src/app/shared/componenents/chapter-navigation/chapter-navigation.component.ts
@@ -0,0 +1,58 @@
+import {Component, effect, inject, OnInit} from "@angular/core";
+import {ChapterNavigationService} from "@shared/componenents/chapter-navigation/services/chapter-navigation.service";
+import {NgClass} from "@angular/common";
+import {ChapterTDE, CoursesInstance} from "@core/constants/chapters.constants";
+import {RouterLink} from "@angular/router";
+import {
+	NAV_DEMO,
+	NAV_EXERCISE,
+	NAV_THEORY,
+} from "@core/constants/navigation.constants";
+
+@Component({
+	selector: "chapter-navigation",
+	imports: [NgClass, RouterLink],
+	templateUrl: "./chapter-navigation.component.html",
+	styleUrl: "./chapter-navigation.component.scss",
+})
+export class ChapterNavigationComponent {
+	protected readonly ChapterTDE = ChapterTDE;
+
+	private readonly _chapterNav: ChapterNavigationService = inject(
+		ChapterNavigationService,
+	);
+
+	/* current chapter */
+	currentIndex = this._chapterNav.currentIndex;
+	currentCategory = this._chapterNav.currentCategory;
+	hasTheory = false;
+	hasDemo = false;
+	hasExercise = false;
+
+	/* previous chapter */
+	previousChapter = this._chapterNav.previousChapter;
+	previousChapterName = this._chapterNav.previousChapterName;
+
+	/* next chapter */
+	nextChapter = this._chapterNav.nextChapter;
+	nextChapterName = this._chapterNav.nextChapterName;
+
+	constructor() {
+		let currIndex = this._chapterNav.currentIndex();
+		if (currIndex != null) {
+			this.hasTheory = CoursesInstance.doesChapterIdHas(currIndex, [
+				ChapterTDE.theory,
+			]);
+			this.hasDemo = CoursesInstance.doesChapterIdHas(currIndex, [
+				ChapterTDE.demo,
+			]);
+			this.hasExercise = CoursesInstance.doesChapterIdHas(currIndex, [
+				ChapterTDE.exercise,
+			]);
+		}
+	}
+
+	protected readonly NAV_THEORY = NAV_THEORY;
+	protected readonly NAV_DEMO = NAV_DEMO;
+	protected readonly NAV_EXERCISE = NAV_EXERCISE;
+}
---
+++ src/app/shared/componenents/chapter-navigation/services/chapter-navigation.service.ts
@@ -0,0 +1,117 @@
+import {inject, Injectable, signal} from "@angular/core";
+import {
+	Chapter,
+	ChapterTDE,
+	CoursesInstance,
+} from "@core/constants/chapters.constants";
+import {Router} from "@angular/router";
+
+@Injectable({
+	providedIn: "root",
+})
+export class ChapterNavigationService {
+	private _router: Router = inject(Router);
+
+	currentCategory = signal<ChapterTDE | null>(null);
+	currentIndex = signal<number | null>(null);
+	currentChapterName = signal<string | null>(null);
+
+	previousChapter = signal<number | null>(null);
+	previousChapterName = signal<string | null>(null);
+	nextChapter = signal<number | null>(null);
+	nextChapterName = signal<string | null>(null);
+
+	constructor() {
+		this.checkPage();
+		this._router.events.subscribe(() => {
+			this.checkPage();
+		});
+	}
+
+	checkPage() {
+		const url = this._router.url.split("/");
+		const category: string = url[1];
+
+		if (category in ChapterTDE) {
+			this.currentCategory.set(
+				ChapterTDE[category as keyof typeof ChapterTDE],
+			);
+			const currIndex = Number(url[2]);
+			this.currentIndex.set(currIndex);
+
+			if (currIndex != null) {
+				this.currentChapterName.set(
+					CoursesInstance.chapterOrder[currIndex].name || null,
+				);
+
+				const prevChap = this.checkPreviousChapter();
+				this.previousChapter.set(prevChap?.index || null);
+				this.previousChapterName.set(prevChap?.chap.name || null);
+
+				const nextChap = this.checkNextChapter();
+				this.nextChapter.set(nextChap?.index || null);
+				this.nextChapterName.set(nextChap?.chap.name || null);
+			}
+		}
+	}
+
+	private checkPreviousChapter(): {chap: Chapter; index: number} | null {
+		const currCategory = this.currentCategory();
+		const currIndex = this.currentIndex();
+
+		// if we don't have a current category or index, we can't determine if there is a previous chapter
+		if (currCategory == null || currIndex == null) {
+			return null;
+		}
+
+		// if we are at the first chapter, there is no previous chapter
+		let found: Chapter | null = null;
+		let foundIndex: number | null = null;
+		for (let i = currIndex - 1; i >= 0 && found == null; i--) {
+			if (CoursesInstance.chapterOrder[i].has.includes(currCategory)) {
+				found = CoursesInstance.chapterOrder[i];
+				foundIndex = i;
+			}
+		}
+
+		if (found !== null && foundIndex !== null) {
+			return {
+				chap: found,
+				index: foundIndex,
+			};
+		}
+		return null;
+	}
+
+	private checkNextChapter(): {chap: Chapter; index: number} | null {
+		const currCategory = this.currentCategory();
+		const currIndex = this.currentIndex();
+
+		// if we don't have a current category or index, we can't determine if there is a next chapter
+		if (currCategory == null || currIndex == null) {
+			return null;
+		}
+
+		// if we are at the last chapter, there is no next chapter
+		let found: Chapter | null = null;
+		let foundIndex: number | null = null;
+		for (
+			let i = currIndex + 1;
+			i < CoursesInstance.chapterOrder.length && found == null;
+			i++
+		) {
+			if (CoursesInstance.chapterOrder[i].has.includes(currCategory)) {
+				found = CoursesInstance.chapterOrder[i];
+				foundIndex = i;
+			}
+		}
+
+		if (found !== null && foundIndex !== null) {
+			return {
+				chap: found,
+				index: foundIndex,
+			};
+		}
+		return null;
+	}
+}
