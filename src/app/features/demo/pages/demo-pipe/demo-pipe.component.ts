dfc2b31fcc574482383ef9f55b2f2a7f0618c53c
phil-bstorm <philippe.haerens@bstorm.be>
2025-04-01 21:08:44 +0200

    [Add] Pipes

--- src/app/features/demo/pages/demo-pipe/demo-pipe.component.ts
+++ src/app/features/demo/pages/demo-pipe/demo-pipe.component.ts
@@ -1,9 +1,15 @@
 import {Component} from "@angular/core";
 import {DemoHeaderComponent} from "@app/features/demo/components/layout/demo-header/demo-header.component";
+import {DemoIntegratedPipesComponent} from "@app/features/demo/pages/demo-pipe/demo-integrated-pipes/demo-integrated-pipes.component";
+import {DemoCustomPipeComponent} from "@app/features/demo/pages/demo-pipe/demo-custom-pipe/demo-custom-pipe.component";
 
 @Component({
 	selector: "demo-pipe",
-	imports: [DemoHeaderComponent],
+	imports: [
+		DemoHeaderComponent,
+		DemoIntegratedPipesComponent,
+		DemoCustomPipeComponent,
+	],
 	templateUrl: "./demo-pipe.component.html",
 	styleUrl: "./demo-pipe.component.scss",
 })
