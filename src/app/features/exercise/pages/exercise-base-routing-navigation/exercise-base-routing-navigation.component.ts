0c29ffaa482a73cc60c09340fd826e6b17a399c6
phil-bstorm <philippe.haerens@bstorm.be>
2025-04-01 15:08:43 +0200

    [Refactor] exercise-header

--- src/app/features/exercise/pages/exercise-base-routing-navigation/exercise-base-routing-navigation.component.ts
+++ src/app/features/exercise/pages/exercise-base-routing-navigation/exercise-base-routing-navigation.component.ts
@@ -1,17 +1,17 @@
 import {Component, inject} from "@angular/core";
 import {SpoilComponent} from "@shared/components/spoil/spoil.component";
-import {ChapterNavigationComponent} from "@shared/components/chapter-navigation/chapter-navigation.component";
 import {ChapterNavigationService} from "@shared/components/chapter-navigation/services/chapter-navigation.service";
 import {RouterLink} from "@angular/router";
 import {HighlightJsDirective} from "ngx-highlight-js";
+import {ExerciseHeaderComponent} from "@app/features/exercise/components/layout/exercise-header/exercise-header.component";
 
 @Component({
 	selector: "exercise-base-routing-navigation",
 	imports: [
 		SpoilComponent,
-		ChapterNavigationComponent,
 		RouterLink,
 		HighlightJsDirective,
+		ExerciseHeaderComponent,
 	],
 	templateUrl: "./exercise-base-routing-navigation.component.html",
 	styleUrl: "./exercise-base-routing-navigation.component.scss",
--- src/app/features/exercise/pages/exercise-composant/exercise-composant.component.ts
+++ src/app/features/exercise/pages/exercise-composant/exercise-composant.component.ts
@@ -1,16 +1,16 @@
 import {Component} from "@angular/core";
-import {ChapterNavigationComponent} from "@shared/components/chapter-navigation/chapter-navigation.component";
 import {SpoilComponent} from "@shared/components/spoil/spoil.component";
 import {AnimalerieComponent} from "@app/features/exercise/pages/exercise-composant/animalerie/animalerie.component";
 import {HighlightJsDirective} from "ngx-highlight-js";
+import {ExerciseHeaderComponent} from "@app/features/exercise/components/layout/exercise-header/exercise-header.component";
 
 @Component({
 	selector: "exercise-composant",
 	imports: [
-		ChapterNavigationComponent,
 		SpoilComponent,
 		AnimalerieComponent,
 		HighlightJsDirective,
+		ExerciseHeaderComponent,
 	],
 	templateUrl: "./exercise-composant.component.html",
 	styleUrl: "./exercise-composant.component.scss",
