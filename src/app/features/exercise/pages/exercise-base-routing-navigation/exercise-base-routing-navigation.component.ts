5d6ef94291404b57345e593d29107c054724efbf
phil-bstorm <philippe.haerens@bstorm.be>
2025-04-08 10:07:36 +0200

    [Remove] Hint

--- src/app/features/exercise/pages/exercise-base-routing-navigation/exercise-base-routing-navigation.component.ts
+++ src/app/features/exercise/pages/exercise-base-routing-navigation/exercise-base-routing-navigation.component.ts
@@ -2,17 +2,11 @@ import {Component, inject} from "@angular/core";
 import {SpoilComponent} from "@shared/components/spoil/spoil.component";
 import {ChapterNavigationService} from "@shared/components/chapter-navigation/services/chapter-navigation.service";
 import {RouterLink} from "@angular/router";
-import {HighlightJsDirective} from "ngx-highlight-js";
 import {ExerciseHeaderComponent} from "@app/features/exercise/components/layout/exercise-header/exercise-header.component";
 
 @Component({
 	selector: "exercise-base-routing-navigation",
-	imports: [
-		SpoilComponent,
-		RouterLink,
-		HighlightJsDirective,
-		ExerciseHeaderComponent,
-	],
+	imports: [SpoilComponent, RouterLink, ExerciseHeaderComponent],
 	templateUrl: "./exercise-base-routing-navigation.component.html",
 	styleUrl: "./exercise-base-routing-navigation.component.scss",
 })
