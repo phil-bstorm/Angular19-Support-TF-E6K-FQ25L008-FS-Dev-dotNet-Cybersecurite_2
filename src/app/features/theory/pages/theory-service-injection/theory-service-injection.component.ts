243f97935886e66232405f00ef8d61de7e7c62d6
phil-bstorm <philippe.haerens@bstorm.be>
2025-04-08 15:47:31 +0200

    [Add] Theory : service injection

--- src/app/features/theory/pages/theory-service-injection/theory-service-injection.component.ts
+++ src/app/features/theory/pages/theory-service-injection/theory-service-injection.component.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
 import {TheoryHeaderComponent} from "@app/features/theory/components/layout/theory-header/theory-header.component";
+import {HighlightJsDirective} from "ngx-highlight-js";
 
 @Component({
 	selector: "theory-service-injection",
-	imports: [TheoryHeaderComponent],
+	imports: [TheoryHeaderComponent, HighlightJsDirective],
 	templateUrl: "./theory-service-injection.component.html",
 	styleUrl: "./theory-service-injection.component.scss",
 })
