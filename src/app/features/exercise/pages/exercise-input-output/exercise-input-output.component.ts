880a2cb875582d9663a10ac4dc1d516babace090
phil-bstorm <philippe.haerens@bstorm.be>
2025-04-08 11:10:08 +0200

    [Add] Input output

--- src/app/features/exercise/pages/exercise-input-output/exercise-input-output.component.ts
+++ src/app/features/exercise/pages/exercise-input-output/exercise-input-output.component.ts
@@ -1,9 +1,15 @@
 import {Component} from "@angular/core";
 import {ExerciseHeaderComponent} from "@app/features/exercise/components/layout/exercise-header/exercise-header.component";
+import {InputOutputExercise1Component} from "@app/features/exercise/pages/exercise-input-output/components/input-output-exercise1/input-output-exercise1.component";
+import {InputOutputExercise2Component} from "@app/features/exercise/pages/exercise-input-output/components/input-output-exercise2/input-output-exercise2.component";
 
 @Component({
 	selector: "exercise-input-output",
-	imports: [ExerciseHeaderComponent],
+	imports: [
+		ExerciseHeaderComponent,
+		InputOutputExercise1Component,
+		InputOutputExercise2Component,
+	],
 	templateUrl: "./exercise-input-output.component.html",
 	styleUrl: "./exercise-input-output.component.scss",
 })
--- src/app/features/theory/pages/theory-input-output/theory-input-output.component.ts
+++ src/app/features/theory/pages/theory-input-output/theory-input-output.component.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
 import {TheoryHeaderComponent} from "@app/features/theory/components/layout/theory-header/theory-header.component";
+import {HighlightJsDirective} from "ngx-highlight-js";
 
 @Component({
 	selector: "theory-input-output",
-	imports: [TheoryHeaderComponent],
+	imports: [TheoryHeaderComponent, HighlightJsDirective],
 	templateUrl: "./theory-input-output.component.html",
 	styleUrl: "./theory-input-output.component.scss",
 })
