1cb3a37f04abd1d378b1356979a97d2e26f5b5d5
phil-bstorm <philippe.haerens@bstorm.be>
2025-04-09 14:43:32 +0200

    [Add] Theory + Demo FormArray

--- src/app/features/demo/pages/demo-formulaire/demo-formulaire.component.ts
+++ src/app/features/demo/pages/demo-formulaire/demo-formulaire.component.ts
@@ -16,7 +16,15 @@ import {DemoHeaderComponent} from "@app/features/demo/components/layout/demo-hea
 export class DemoFormulaireComponent {
 	private readonly _fb = inject(FormBuilder);
 
-	// Formulaire de contact
+	// addreses du formulaire de contact
+	addresses = this._fb.array([
+		this._fb.group({
+			street: ["", [Validators.required]],
+			city: ["", [Validators.required]],
+		}),
+	]);
+
+	// formulaire de contact
 	contactForm = this._fb.group({
 		email: new FormControl("", [Validators.required, Validators.email]),
 		firstName: this._fb.control("", [
@@ -24,7 +32,23 @@ export class DemoFormulaireComponent {
 			Validators.minLength(2),
 		]),
 		lastName: ["", [Validators.required, Validators.minLength(2)]],
+		addresses: this.addresses,
 	});
+
+	addAddress() {
+		this.addresses.push(
+			this._fb.group({
+				street: ["", [Validators.required]],
+				city: ["", [Validators.required]],
+			}),
+		);
+	}
+
+	removeAddress(index: number) {
+		if (index > 0) {
+			this.addresses.removeAt(index);
+		}
+	}
 
 	onSubmit() {
 		if (this.contactForm.valid) {
