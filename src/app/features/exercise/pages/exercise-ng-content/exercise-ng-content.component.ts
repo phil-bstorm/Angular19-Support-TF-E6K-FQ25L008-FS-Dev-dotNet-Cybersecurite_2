2d5d5cec1b032ce85c444ebf60379679ab1a8983
phil-bstorm <philippe.haerens@bstorm.be>
2025-04-09 09:11:07 +0200

    Remove unused import

--- src/app/features/exercise/pages/exercise-ng-content/exercise-ng-content.component.ts
+++ src/app/features/exercise/pages/exercise-ng-content/exercise-ng-content.component.ts
@@ -2,16 +2,10 @@ import {Component} from "@angular/core";
 import {ExerciseHeaderComponent} from "@app/features/exercise/components/layout/exercise-header/exercise-header.component";
 import {SpoilComponent} from "@shared/components/spoil/spoil.component";
 import {CadrePhotoComponent} from "@app/features/exercise/pages/exercise-ng-content/cadre-photo/cadre-photo.component";
-import {HighlightJsDirective} from "ngx-highlight-js";
 
 @Component({
 	selector: "exercise-ng-content",
-	imports: [
-		ExerciseHeaderComponent,
-		SpoilComponent,
-		CadrePhotoComponent,
-		HighlightJsDirective,
-	],
+	imports: [ExerciseHeaderComponent, SpoilComponent, CadrePhotoComponent],
 	templateUrl: "./exercise-ng-content.component.html",
 	styleUrl: "./exercise-ng-content.component.scss",
 })
