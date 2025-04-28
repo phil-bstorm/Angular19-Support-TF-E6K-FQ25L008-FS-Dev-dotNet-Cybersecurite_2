43ec4b9b27f78bbdb34b51c4f8b7863f7d1ced9a
phil-bstorm <philippe.haerens@bstorm.be>
2025-04-28 14:54:22 +0200

    [Add] Theory + Demo : Signal

--- src/app/features/theory/pages/theory-signal/theory-signal.component.ts
+++ src/app/features/theory/pages/theory-signal/theory-signal.component.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
 import {TheoryHeaderComponent} from "@app/features/theory/components/layout/theory-header/theory-header.component";
+import {HighlightJsDirective} from "ngx-highlight-js";
 
 @Component({
 	selector: "theory-signal",
-	imports: [TheoryHeaderComponent],
+	imports: [TheoryHeaderComponent, HighlightJsDirective],
 	templateUrl: "./theory-signal.component.html",
 	styleUrl: "./theory-signal.component.scss",
 })
